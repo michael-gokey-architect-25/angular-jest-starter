e19b48b49dc63411e241fb3950909338d8454f80
michael-gokey-architect <michael.gokey.architect@gmail.com>
2025-10-28 14:48:43 -0400

    trying to refine the two versions, between stack and GitHub

--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,1 +1,12 @@
+// src/setup-jest.ts
 import 'jest-preset-angular/setup-jest';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
